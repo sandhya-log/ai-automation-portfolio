479954d3572502252104f86f79f0d15ff4386198
sandhya-log <hellosandhya11@gmail.com>
2025-12-12 23:54:53 +0530

    database

--- backend-projects/fastapi-user-auth-system/app/main.py
+++ backend-projects/fastapi-user-auth-system/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from app.routers import auth
+from app.db.database import Base, engine
+
 app = FastAPI(title="FastAPI Auth Project")
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 
