887e32fd03899474e3f1bf8677dd2db86897a294
sandhya-log <hellosandhya11@gmail.com>
2025-12-18 04:09:09 +0530

    page added

---
+++ backend-projects/fastapi-user-auth-system/app/core/security.py
@@ -0,0 +1,49 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose import jwt, JWTError
+from sqlalchemy.orm import Session
+
+from app.core.config import settings
+from app.db.database import get_db
+from app.models.user import User
+
+security = HTTPBearer()
+
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+    db: Session = Depends(get_db)
+):
+    # Extract JWT token
+    token = credentials.credentials
+
+    try:
+        # Decode the JWT token
+        payload = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM]
+        )
+
+        # Extract email from token payload
+        email: str = payload.get("sub")
+        if email is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid token payload"
+            )
+
+        # Fetch the user from the database
+        user = db.query(User).filter(User.email == email).first()
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="User not found"
+            )
+
+        return user
+
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token"
+        )
--- backend-projects/fastapi-user-auth-system/app/routers/auth.py
+++ backend-projects/fastapi-user-auth-system/app/routers/auth.py
@@ -1,6 +1,57 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from app.schemas.user import UserCreate, UserResponse
+from app.models.user import User
+from app.db.database import get_db
+from app.utils.hashing import hash_password, verify_password
+from app.utils.jwt_handler import create_access_token
+from app.schemas.user import UserLogin
 
 router = APIRouter()
+
+@router.post("/signup", response_model=UserResponse)
+def signup(user: UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(
+            status_code= status.HTTP_400_BAD_REQUEST,
+            detail="Email alreay exist"
+        )
+    
+    hashed_pw = hash_password(user.password)
+
+    new_user = User(
+        email = user.email,
+        hashed_password = hashed_pw 
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid email or password"
+        )
+    
+    if not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Invalid email or password"
+        )
+    
+    access_token = create_access_token({"sub": db_user.email})
+
+    return {"access_token"  : access_token, "token_type": "bearer"}
+
 
 @router.get("/ping")
 def ping():
